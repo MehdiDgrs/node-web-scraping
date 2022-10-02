7349be53bf445f0f54f1760040a6b9ae42b48bef
MehdiDgrs <Mehdigital@outlook.com>
2022-10-02 20:16:22 +0200

    :tada:

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const axios = require('axios');
 let fs = require ('fs')
+const cheerio = require('cheerio')
+
+let resultArray = []
+let fetchData = async  (url) => {
+    let result = await axios.get(url)
+   
+    return cheerio.load(result.data)
+    
+}
+let fetchDataHash = async () => {
+
+let $ =  await fetchData('https://www.nowinstock.net/videogaming/consoles/sonyps5/');
+let result = $("#trackerContent > div#data > table > tbody >tr").each((index,element)=> {if(index===0) return true;
+let tds = $(element).find('td');
+
+let [title,availability,price,date] = tds
+resultArray.push({
+title:$(title).text(),
+availability:$(availability).text(),
+price:$(price).text(),
+date:$(date).text(),
+
+})
 
 
+})
+console.log(resultArray)
+return resultArray 
+ 
 
-let res = axios({method:'get',url: "https://www.boulanger.com/ref/1147567"}).then((response)=> console.log(response.data))
+}
 
-console.log(res)
+
+fetchDataHash()
